fdb2f1e1b2b173daa1459ea213f2ec3f464bd743
zmj27404 <zmj27404@sina.cn>
2017-09-27 22:47:58 +0800

    增加一些不知道啥

--- src/main/java/cn/codingxiaxw/web/SeckillController.java
+++ src/main/java/cn/codingxiaxw/web/SeckillController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import java.util.Date;
 import java.util.List;
 
@@ -73,7 +74,7 @@ public class SeckillController
             result=new SeckillResult<Exposer>(false,e.getMessage());
         }
 
-        return result;
+                return result;
     }
 
     @RequestMapping(value = "/{seckillId}/{md5}/execution",
